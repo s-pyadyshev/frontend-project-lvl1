ae0b161dc4242564ec4442f0a0c813bfed53c474
Sergei Piadyshev <s.pyadyshev@gmail.com>
2021-12-09 20:42:29 +0400

    add step 3

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from "readline-sync";
+
+export const askUsername = () => {
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+};
